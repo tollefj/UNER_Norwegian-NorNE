94e41e2259b4e404bdbf1281a173fa2e5db1caa4
Tollef Jørgensen <tollefj@gmail.com>
2024-09-24 12:56:18 +0200

    update correct OTH->O map

--- convert.py
+++ convert.py
@@ -7,10 +7,13 @@ from util import get_aligned_file, get_file, lang_map, paths
 logging.basicConfig(level=logging.INFO)
 
 
+file_getters = [get_aligned_file, get_file]
+# file_getters = [get_file]
+
 for lang in paths.keys():
     for split in ["dev", "test", "train"]:
-        for getter in [get_aligned_file, get_file]:
-            identifier = "NorNE" if getter == get_file else "NorNE-aligned"
+        for getter in file_getters:
+            identifier = "NorNE" if getter == get_file else "NorNE-document-aligned"
             OUT_PATH = os.path.join(f"UNER_Norwegian_{identifier}")
             os.makedirs(OUT_PATH, exist_ok=True)
             logging.info(f"Fetching {lang} {split} from {getter.__name__}")
@@ -22,6 +25,7 @@ for lang in paths.keys():
                 logging.info(f"Got {len(data)} sentences")
                 for sentence in data:
                     meta = sentence.metadata
+                    # print(meta)
                     _id = meta["sent_id"]
                     _text = meta["text"]
                     _newdoc = meta.get("newdoc id", None)
@@ -31,10 +35,15 @@ for lang in paths.keys():
                     }
                     if _newdoc:
                         new_meta["newdoc id"] = _newdoc
+                    if "newpar" in meta:
+                        new_meta["newpar"] = True
                     # sort it alphabetically, as text follows sentid follows newdoc/newpar
                     new_meta = dict(sorted(new_meta.items(), key=lambda item: item[0]))
                     for k, v in new_meta.items():
-                        f.write(f"# {k} = {v}\n")
+                        if isinstance(v, bool):
+                            f.write(f"# {k}\n")
+                        else:
+                            f.write(f"# {k} = {v}\n")
 
                     for tok_idx, token_info in enumerate(sentence):
                         norne_tag = token_info["misc"].get("name", "-")
